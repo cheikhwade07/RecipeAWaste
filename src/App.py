9073910a0fdb5c8986f86abf8eafb214f288a359
Seydi Cheikh Wade <seydicheikhwade@gmail.com>
2026-01-30 20:48:17 -0500

    Added App.py

---
+++ src/App.py
@@ -0,0 +1,16 @@
+from datetime import date
+
+class FoodItem:
+    quantity = 0
+    def __init__(self, name: str, calorie: int, protein: int, expiry_date: date):
+        self.name = name
+        self.calorie = calorie
+        self.protein = protein
+        self.expiry_date = expiry_date
+        FoodItem.quantity = 0
+
+    def increase_quantity(self):
+        FoodItem.quantity += 1
+
+    def decrease_quantity(self):
+        FoodItem.quantity -= 1
