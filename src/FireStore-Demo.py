684792733bd9de0b80d97107759bf11979abcbea
makername-adam <113399262+makername-adam@users.noreply.github.com>
2026-01-31 20:16:58 -0500

    add firestore demo & json to gitignore

---
+++ src/FireStore-Demo.py
@@ -0,0 +1,21 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from datetime import datetime
+cred = credentials.Certificate("C:\\Users\\adamt\\OneDrive - Carleton University\\MastersDegreeCarleton\\Year1\\AHivers\\HackTheFuture_MSA\\firebase.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+data = {
+    'name': 'Apple',
+    'calorie': 95,
+    'protein': 0,
+    'carbs': 25,
+    'fat': 0,
+    'expiry_date': '2023/12/31'
+}
+
+doc_ref = db.collection('food_items_collection').document()
+doc_ref.set(data)
+
+print('Document ID:', doc_ref.id)
