955802b33ed5625935560e43198b58b2a7fa4852
Moses Hmung <mosesvlhmung@gmail.com>
2026-02-02 15:30:04 -0500

    finished most of the html, created reciple page with js code and credits page
    # Conflicts:
    #	RecipeAWaste/public/index.html
    #	RecipeAWaste/public/nutrition.html

--- RecipeAWaste/public/ingredients.js
+++ RecipeAWaste/public/ingredients.js
@@ -19,3 +19,22 @@ function add() {
     document.getElementById('ingredient').value = '';
     document.getElementById('quantity').value = '';
 }
+
+function getIngredients(){
+    const table = document.getElementById("ingredient-table-body");
+    const ingredients = [];
+    for (let row of table.rows) {
+        const ingredient = row.cells[0].innerText;
+        const quantity = row.cells[1].innerText;
+        ingredients.push({ ingredient, quantity });
+    }
+    return ingredients;
+}
+
+function generateRecipe(){
+    const ingredients = getIngredients();
+    
+   
+    localStorage.setItem("ingredients", JSON.stringify(ingredients));
+    window.location.href = "recipes.html";
+}
